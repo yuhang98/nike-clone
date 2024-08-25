7e576c2122311d637965ad36f8134d06af3e7d77
Yu Hang Lee <lyu8572@gmail.com>
2024-08-25 15:33:41 +0800

    Subscribe and Footer Component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,6 +6,7 @@ const Button = ({
   backgroundColor,
   textColor,
   borderColor,
+  fullWidth,
 }) => {
   return (
     <button
@@ -15,7 +16,7 @@ const Button = ({
        backgroundColor
          ? `${backgroundColor}  ${textColor} ${borderColor}`
          : 'bg-coral-red text-white border-coral-red'
-     } rounded-full w-full"}`}
+     } rounded-full ${fullWidth && 'w-full'}"}`}
     >
       {label}
       {iconURL && (
